db98909089f30a9e131c44ae5fd04c682d603957
Jan Vansteenkiste <jan@vstone.eu>
2017-01-31 08:57:25 +0100

    Easy access to fixtures dir from spec tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
 require 'puppet_rake_tasks'
+
+FIXTURES_ROOT = File.join(File.dirname(__FILE__), "fixtures") unless defined?(FIXTURES_ROOT)
