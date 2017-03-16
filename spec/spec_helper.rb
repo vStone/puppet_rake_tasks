ebfa7e5ad01978b792a469f4fc0058ed5536c606
Jan Vansteenkiste <jan@vstone.eu>
2017-03-16 06:04:03 +0100

    Style fixes in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,4 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 
 require 'puppet_rake_tasks'
 
-FIXTURES_ROOT = File.join(File.dirname(__FILE__), "fixtures") unless defined?(FIXTURES_ROOT)
+FIXTURES_ROOT = File.join(File.dirname(__FILE__), 'fixtures') unless defined?(FIXTURES_ROOT)
