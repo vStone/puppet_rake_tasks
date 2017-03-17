a8e18e5301130a39151c202722cc22478274bcb2
Jan Vansteenkiste <jan@vstone.eu>
2017-03-17 09:24:50 +0100

    Update documentation for the resolver

--- lib/puppet_rake_tasks/depchecker/resolver.rb
+++ lib/puppet_rake_tasks/depchecker/resolver.rb
@@ -5,7 +5,8 @@ require_relative 'helpers'
 
 module PuppetRakeTasks
   module DepChecker
-    # Class that can detect all puppet module dependency issues.
+    # Class to detect all puppet module dependency issues.
+    # It uses puppet module tool internals to detect all issues.
     class Resolver
       include Helpers
 
@@ -13,10 +14,14 @@ module PuppetRakeTasks
         @modulepath = Helpers.normalize_path(module_path)
       end
 
+      # Returns the puppet module tool environment.
+      # @see http://www.rubydoc.info/gems/puppet/Puppet/ModuleTool#environment_from_options-class_method
       def env
         @env ||= Puppet::ModuleTool.environment_from_options(modulepath: modulepath_s)
       end
 
+      # Collect the installed modules using Puppet::ModuleTool.
+      # @see http://www.rubydoc.info/gems/puppet/Puppet/ModuleTool/InstalledModules
       def modules
         @modules ||= Puppet::ModuleTool::InstalledModules.new(env)
       end
@@ -25,19 +30,25 @@ module PuppetRakeTasks
       def reset_caches
         @env = nil
         @modules = nil
+        @incidents = nil
       end
 
-      # Set or default module path.
+      # Returns the configured module paths.
+      # @return [Array<String>] configured module paths as an array.
       def modulepath
         @modulepath ||= ['.']
       end
 
       # Return modulepath as a string
+      # @return [String] module paths joined with `File::PATH_SEPARATOR`
       def modulepath_s
         @modulepath.flatten.join(File::PATH_SEPARATOR)
       end
 
-      # Set the modulepath
+      # Set the modulepath and normalize using {Helpers#normalize_path}
+      # When the module path changes, cached values are {#reset_caches cleared}
+      # @param [Array<String>,String] path Module paths
+      # @return [Array<String>] configured and normalized module paths.
       def modulepath=(path)
         unless path == @modulepath
           # reset the env and loaded modules when changing the modulepath
