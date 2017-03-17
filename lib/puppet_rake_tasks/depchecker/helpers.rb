abb575b2acb5ab820813d3db410fcf78719d062b
Jan Vansteenkiste <jan@vstone.eu>
2017-03-17 08:46:28 +0100

    Add documentation to the helpers module

--- lib/puppet_rake_tasks/depchecker/helpers.rb
+++ lib/puppet_rake_tasks/depchecker/helpers.rb
@@ -1,8 +1,13 @@
 module PuppetRakeTasks
   module DepChecker
-    # General helpers
+    # General helpers for various methods.
     module Helpers
       class << self
+
+        # Normalize paths split by `File::PATH_SEPARATOR` by making sure it always
+        # returns an array of paths.
+        # @param path [String,Array] A string or an array with paths.
+        # @return [Array] The normalized paths
         def normalize_path(path)
           if path.is_a?(String)
             path.split(File::PATH_SEPARATOR)
@@ -11,6 +16,11 @@ module PuppetRakeTasks
           end
         end
 
+        # Compare values by converting symbols to strings or by using
+        # regexes to match if the ignore_value is one.
+        # @param ignore_value [String,Regex,Symbol] Value to compare
+        # @param incident_value [String, Symbol] Value to compare against.
+        # @return [Boolean] true or false depending on the equality-ish-ness.
         def compare_values(ignore_value, incident_value)
           if ignore_value.is_a?(Regexp)
             !ignore_value.match(incident_value.to_s).nil?
