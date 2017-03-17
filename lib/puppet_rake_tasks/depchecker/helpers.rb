939329c7bc2b011ced5851fc64b86715a4c928b7
Jan Vansteenkiste <jan@vstone.eu>
2017-03-17 08:24:10 +0100

    Convert helpers to a module for easier re-use

--- lib/puppet_rake_tasks/depchecker/helpers.rb
+++ lib/puppet_rake_tasks/depchecker/helpers.rb
@@ -1,15 +1,23 @@
 module PuppetRakeTasks
   module DepChecker
-    class Resolver
-      # General helpers
-      module Helpers
-        class << self
-          def normalize_path(path)
-            if path.is_a?(String)
-              path.split(File::PATH_SEPARATOR)
-            else
-              [path].flatten
-            end
+    # General helpers
+    module Helpers
+      class << self
+        def normalize_path(path)
+          if path.is_a?(String)
+            path.split(File::PATH_SEPARATOR)
+          else
+            [path].flatten
+          end
+        end
+
+        def compare_values(ignore_value, incident_value)
+          if ignore_value.is_a?(Regexp)
+            !ignore_value.match(incident_value.to_s).nil?
+          else
+            incident_value = incident_value.is_a?(Symbol) ? incident_value.to_s : incident_value
+            ignore_value = ignore_value.is_a?(Symbol) ? ignore_value.to_s : ignore_value
+            incident_value == ignore_value
           end
         end
       end
